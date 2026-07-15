9b352703f1480b51377beb3708791fa68326ae6e
k Rahul Pandit <rahulpandit3154@gmail.com>
2026-07-16 00:48:43 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [paragraph, setParagraph] = useState("")
+  function para(){
+    setParagraph("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={para}>click me</button>
+      <p>{paragraph}</p>
     </div>
   );
 }
